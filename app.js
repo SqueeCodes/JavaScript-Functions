8161a4e74b72655c3aa494537d3767449ed2234d
David <itsmesquee@gmail.com>
2024-06-17 11:55:40 -0600

    created function to check if number is greater than 100

--- app.js
+++ app.js
@@ -3,7 +3,18 @@
 // }
 // console.log(calcPerimeter(10,20));
 
-function calcArea(base, height) {
-    return 0.5 * base * height
+// function calcArea(base, height) {
+//     return 0.5 * base * height
+// }
+// console.log(calcArea(20, 20))
+
+
+// function addTOString(string) {
+//     return string + 'frontend'
+// }
+// console.log(addTOString('apple'));
+
+function sumGreaterThan100(num1, num2) {
+    return num1 + num2 > 100
 }
-console.log(calcArea(3, 2))
+console.log(sumGreaterThan100(20, 10))
