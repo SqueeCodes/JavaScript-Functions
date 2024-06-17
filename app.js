731f597728a3f55dd0701289052ebcdfc99fea83
David <itsmesquee@gmail.com>
2024-06-17 11:39:55 -0600

    created function to convert hours into seconds

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-function addition(num1, num2) {
-    return num1 + num2
+function hoursIntoSeconds(hour) {
+    let minutes = hour * 60;
+    let seconds = minutes * 60;
+    return seconds
 }
-console.log(addition(3, 5))
+console.log(hoursIntoSeconds(2))
+
