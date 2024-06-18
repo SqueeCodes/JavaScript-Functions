03a551b506b76cb0a93e13580da9f81606f5c1b8
David <itsmesquee@gmail.com>
2024-06-18 12:03:28 -0600

    created function that sorts whole numbers low -> high

--- app.js
+++ app.js
@@ -173,18 +173,23 @@
 
 "advanced challenges"
 
-function showRating(rating) {
-    let ratings = "";
-    for ( let i = 0; i < Math.floor(rating); ++i) {
-        ratings += "*";
-        if (i !== Math.floor(rating) -1) {
-            ratings = ratings + " ";
+// function showRating(rating) {
+//     let ratings = "";
+//     for ( let i = 0; i < Math.floor(rating); ++i) {
+//         ratings += "*";
+//         if (i !== Math.floor(rating) -1) {
+//             ratings = ratings + " ";
 
-        }
-    }
-    if (!Number.isInteger(rating)) {
-        ratings += " ."
-    }
-    return ratings
+//         }
+//     }
+//     if (!Number.isInteger(rating)) {
+//         ratings += " ."
+//     }
+//     return ratings
+// }
+// console.log(showRating(3.5));
+
+function sortLowToHigh(numbers) {
+        return numbers.sort((a, b) => a - b);
 }
-console.log(showRating(3.5));
+console.log(sortLowToHigh([1, 5, 0, 10, 4, 69]))
