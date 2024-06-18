757935c09c972e921447f0acefa369f6e4c7d137
David <itsmesquee@gmail.com>
2024-06-18 10:30:28 -0600

    add for loop method to remove falsy components

--- app.js
+++ app.js
@@ -148,7 +148,17 @@
 // }
 // console.log(removeApples(['banana', 'apple', 'ornch', 'apple']));
 
+// function removeFalsy(arr) {
+//     return arr.filter(elem => !!elem === true)
+// }
+
 function removeFalsy(arr) {
-    return arr.filter(elem => !!elem === true)
+    let truthyArr = [];
+    for (let i = 0; i < arr.length; ++i) {
+        if (!!arr[i] === true) {
+            truthyArr.push(arr[i]);
+        }
+    }
+    return truthyArr;
 }
 console.log(removeFalsy(['banana', 'ornch', 'apple', false, '', null, undefined, NaN]))
